10239c1185be0d7e262447384c1da3587ae5b4da
Guilherme Menezes <guilhermemenezes@MacBook-Air-de-Guilherme.local>
2023-08-21 17:20:51 -0300

    Subindo mais exercicios

--- Aula_4/exercicio_17.py
+++
@@ -1,3 +0,0 @@
-ano = input("Digite o ano: ")
-mes = input("Digite o mes: ")
-dia = input("Digite o dia: ")
--- Aula_4/exercicio_18.py
+++ Aula_4/exercicio_18.py
@@ -3,17 +3,17 @@ saque_deposito = input("1 - Realizar saque: \n2 - Realizar depósito: \n")
 
 valor = 0
 if saque_deposito == "1":
-    valor = float(input("Digite o valor do saque:      "))
+    valor = float(input("Digite o valor do saque: "))
     if valor > saldo:
-        print("Operação não permitida")
+        print("Operação realizada com insucesso \n Saldo insuficiente")
     else:
-        print("Operaçao realizada com sucesso")
+        print("Operaçao realizada com sucesso \n Saldo atual: ", saldo - valor, "R$")
 if saque_deposito == "2":
     valor = float(input("Digite o valor do depósito: "))
     if saque_deposito == "2" and valor > 300:
-        print("Operação não permitida")
+        print("Operação realizada com insucesso \n Valor máximo para depósito: 300 R$")
     else:
-        print("Operaçao realizada com sucesso ")
+        print("Operaçao realizada com sucesso \n Saldo atual: ", saldo + valor, "R$ ")
 
 
 
